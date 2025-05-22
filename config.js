a2878ab95f8404fdeeaede8d34349f539edcd2d6
MOMED988 <hkra85174@gmail.com>
2025-05-22 18:21:29 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+module.exports = {
+   botToken: "7222347008:AAFtrH8rE3FjSEYdBT4J4YEiIzn7zCzViiE", // Modify your bot TOKEN here
+   hostURL: "https://open-cemn.onrender.com", // Modify your host URL here
+};
+// for port forewording use "ssh -R 80:localhost:5000 serveo.net"
